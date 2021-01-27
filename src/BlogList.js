26a65c8f9fe8fef8a6c6d67ae615e847bffb7707
mark <markpackham1@gmail.com>
2021-01-27 15:54:04 +0000

    test useEffect, https://youtu.be/jQc_bTFZ5_I?t=32

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,15 +1,12 @@
-const BlogList = ({ blogs, title }) => {
-  //   const blogs = props.blogs;
-  //   const title = props.title;
-
+const BlogList = ({ blogs, title, handleDelete }) => {
   return (
-    <div className="bloglist">
+    <div className="blog-list">
       <h2>{title}</h2>
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
-          <button onClick={() => handleDelete(blog.id)}>Delete Blog</button>
+          <button onClick={() => handleDelete(blog.id)}>delete blog</button>
         </div>
       ))}
     </div>
--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -13,13 +13,18 @@ const Home = () => {
     },
   ]);
 
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
+
+  useEffect(() => {
+    console.log(blogs);
+  });
+
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" />
-      <BlogList
-        blogs={blogs.filter((blog) => blog.author === "mario")}
-        title="Mario's blogs"
-      />
+      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
     </div>
   );
 };
